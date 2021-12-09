21322976fe39f627230bf83b3503433ee63974d4
lutzer <lutz@lu-re.de>
2021-12-09 01:19:06 +0100

    added solution day3

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -4,42 +4,78 @@ use aoc_utils::{AoCSolution, run};
 
 struct Solution {}
 
+fn bitstring_to_u32(s: &str) -> u32 {
+    let mut result = 0;
+    for (i,c) in s.chars().rev().enumerate() {
+        if c == '1' {
+            result += (2 as u32).pow(i as u32)
+        }
+    }
+    return result;
+}
+
 impl AoCSolution for Solution  {
     fn part1(&self, input: &String) -> i64 {
         let instructions = input.lines().map(|l| {
-            let words = l.split(" ").collect::<Vec<&str>>();
-            return (words[0], words[1].parse::<u64>().unwrap());
-        }).collect::<Vec<(&str,u64)>>();
+            return l
+        }).collect::<Vec<&str>>();
 
-        let (hpos,depth) = instructions.into_iter().fold((0,0), |(h,d),val| {
-            match val.0 {
-                "forward" => (h+val.1, d),
-                "down" => (h, d+val.1),
-                "up" => (h, d-val.1),
-                _ => (h,d)
+        let number_size = instructions[0].len();
+        let size = instructions.len() as u32;
+
+        let bit_counts : Vec<u32> = instructions.into_iter().fold(vec![0;number_size], |mut acc, curr| {
+            for (i,c) in curr.chars().enumerate() {
+                if c == '1' { acc[i] += 1}
             }
+            return acc
         });
 
-        return (hpos*depth) as i64;
+        let (gamma_rate, epsilon_rate) = bit_counts.into_iter().fold(("".to_string(), "".to_string()),|mut acc,c| {
+            acc.0 = if c > size/2 { acc.0 + "1" } else { acc.0 + "0" };
+            acc.1 = if c <= size/2 { acc.1 + "1" } else { acc.1 + "0" };
+            return acc;
+        });
+
+        return (bitstring_to_u32(&gamma_rate.to_owned()) * bitstring_to_u32(&epsilon_rate.to_owned())) as i64;
     }
     
     fn part2(&self, input: &String) -> i64 {
+
         let instructions = input.lines().map(|l| {
-            let words = l.split(" ").collect::<Vec<&str>>();
-            return (words[0], words[1].parse::<u64>().unwrap());
-        }).collect::<Vec<(&str,u64)>>();
+            return l
+        }).collect::<Vec<&str>>();
 
-        let (hpos,depth,aim) = instructions.into_iter().fold((0,0,0), |(h,d,a),val| {
-            match val.0 {
-                "forward" => (h+val.1, d+a*val.1, a),
-                "down" => (h, d, a+val.1),
-                "up" => (h, d, a-val.1),
-                _ => (h, d, a)
+        let number_size = instructions[0].len();
+
+        let mut oxygen_ratings = instructions.to_vec();
+        for i in 0..number_size {
+            let bit_count = (&oxygen_ratings).into_iter().fold(0, |acc, curr| {
+                if curr.chars().nth(i).unwrap() == '1' { acc + 1 } else { acc } 
+            });
+            let most_common = if bit_count as f32 >= oxygen_ratings.len() as f32/2.0 { '1' } else { '0' };
+            oxygen_ratings.retain(|&x| {
+                return x.chars().nth(i).unwrap() == most_common
+            });
+            if oxygen_ratings.len() == 1 {
+                break;
             }
-        });
+        }
 
+        let mut co2_ratings = instructions.to_vec();
+        for i in 0..number_size {
+            let bit_count = (&co2_ratings).into_iter().fold(0, |acc, curr| {
+                if curr.chars().nth(i).unwrap() == '1' { acc + 1 } else { acc } 
+            });
+            let least_common = if (bit_count as f32) < co2_ratings.len() as f32/2.0 { '1' } else { '0' };
+            co2_ratings.retain(|&x| {
+                return x.chars().nth(i).unwrap() == least_common
+            });
+            if co2_ratings.len() == 1 {
+                break;
+            }
+        }
 
-        return (hpos*depth) as i64;
+        return (bitstring_to_u32(&oxygen_ratings[0].to_owned()) * bitstring_to_u32(&co2_ratings[0].to_owned())) as i64;
     }
 }
 
@@ -59,29 +95,41 @@ mod tests {
   #[test]
   fn test_part_1() {
     let input = String::from(indoc!{"
-        forward 5
-        down 5
-        forward 8
-        up 3
-        down 8
-        forward 2
+    00100
+    11110
+    10110
+    10111
+    10101
+    01111
+    00111
+    11100
+    10000
+    11001
+    00010
+    01010
     "});
 
     let result = (&Solution {}).part1(&input);
-    assert_eq!(result, 150);
+    assert_eq!(result, 198);
   }
 
   #[test]
   fn test_part_2() {
     let input = String::from(indoc!{"
-        forward 5
-        down 5
-        forward 8
-        up 3
-        down 8
-        forward 2
+    00100
+    11110
+    10110
+    10111
+    10101
+    01111
+    00111
+    11100
+    10000
+    11001
+    00010
+    01010
     "});
     let result = (&Solution {}).part2(&input);
-    assert_eq!(result, 900);
+    assert_eq!(result, 230);
   }
 }
